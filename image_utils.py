7cd3127ec53535f2082d67cffca1637ae230a1d1
shellyper <shellyper83@gmail.com>
2025-01-27 14:49:43 +0200

    Update image_utils.py

--- image_utils.py
+++ image_utils.py
@@ -1,9 +1,37 @@
 from PIL import Image
 import numpy as np
 from scipy.signal import convolve2d
+import matplotlib.pyplot as plt
 
-def load_image(path):
-    pass # Replace the `pass` with your code
+def load_image(file_path):
+  
+    image = Image.open(file_path)
+    image_array = np.array(image)
+    return image_array 
 
-def edge_detection(image):
-    pass # Replace the `pass` with your code
+file_path = '/content/lena.jpg'  
+image_array = load_image(file_path)
+
+plt.imshow(image_array, cmap='gray')
+
+
+def edge_detection(image_array):
+   
+    grayscale_image = np.mean(image_array, axis=2)
+
+    kernelY = np.array([
+        [1, 2, 1],
+        [0, 0, 0],
+        [-1, -2, -1]]) 
+     
+    kernelY = np.array([
+        [1, 2, 1],
+        [0, 0, 0],
+        [-1, -2, -1] ])
+    edgeY = convolve2d(grayscale_image, kernelY, mode='same', boundary='fill', fillvalue=0)
+    edgeX = convolve2d(grayscale_image, kernelX, mode='same', boundary='fill', fillvalue=0)
+
+    edgeMAG = np.sqrt(edgeX*2 + edgeY*2)
+
+
+    return edgeMAG
